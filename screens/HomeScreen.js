1464fad53f62bed3826a786aa58a9ec10f0e81fc
Matthew Singh <mattsingh@hotmail.com>
2021-11-22 01:29:19 -0500

    Add icons to tab bar navigator

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,15 +4,40 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MatchScreen from './MatchScreen';
 import ProfileScreen from './ProfileScreen';
 import MessageStack from '../routes/MessageStack';
+import { FontAwesome, Entypo } from 'react-native-vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
 	return (
 		<Tab.Navigator screenOptions={{ headerShown: false }}>
-			<Tab.Screen name='Match' component={MatchScreen} />
-			<Tab.Screen name='Chats' component={MessageStack} />
-			<Tab.Screen name='Profile' component={ProfileScreen} />
+			<Tab.Screen
+				name='Match'
+				component={MatchScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome name='group' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Chats'
+				component={MessageStack}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Entypo name='chat' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Profile'
+				component={ProfileScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome name='user-circle-o' color={color} size={size} />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 }
